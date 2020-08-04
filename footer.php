e695408394be7c076851d0637e0551f9b4247710
mustadev <58852293+mustadev@users.noreply.github.com>
2020-08-04 16:10:04 +0100

    add link to theme repo

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <footer class="footer text-center py-2 theme-bg-dark">
 		   
-            <p class="copyright"><a href="https://youtube.com/FollowAndrew">FollowAndrew</a></p>
+            <p class="copyright"><a href="https://github.com/mustadev/tillas">Tillas</a></p>
             <?php 
                 dynamic_sidebar( 'footer-1' );
             ?>
